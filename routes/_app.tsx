6ab62d6114755155ef8433d7bf2b542a8ec037f5
Dampuzakura <168283013+dampuzakura@users.noreply.github.com>
2024-12-13 20:59:45 +0900

    フォントを追加

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -8,6 +8,8 @@ export default function App({ Component }: PageProps) {
         <title>circle</title>
         <link rel="stylesheet" href="/styles.css" />
         <link rel="stylesheet" href="https://esm.sh/@picocss/pico@2.0.6/css/pico.classless.orange.min.css" />
+        <link rel="preconnect" href="https://fonts.bunny.net" />
+        <link href="https://fonts.bunny.net/css?family=ibm-plex-sans:400|ibm-plex-sans-jp:400" rel="stylesheet" />
       </head>
       <body>
         <Component />
